6b8a332d0a06281b014ac88dfdd6ddacb8af5e3a
Gabriel Lima <89604566+neogabe@users.noreply.github.com>
2025-03-13 23:29:04 +0000

    adiciona setup do projeto e alguns estilos globais

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+    <h1>lorem</h1>
+    <Link href="/">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, quaerat ducimus vero hic fugiat quam repellendus? Perferendis quia consequuntur dolorem hic voluptatum inventore reprehenderit, ea, quis modi quod, minima atque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate laudantium recusandae, atque aliquam ab harum consequatur repellat facere illum quibusdam porro officiis nesciunt error explicabo aliquid laboriosam inventore perferendis voluptates?
+    Voluptate ut ratione officiis cumque dolores fuga perferendis id aliquam autem distinctio similique, totam quaerat incidunt iusto quod doloribus quibusdam perspiciatis explicabo culpa eum iure eaque maiores doloremque! Aliquam, excepturi.
+    At possimus hic qui in excepturi odit libero perspiciatis maiores. Ut similique voluptate quisquam eos iste explicabo soluta nostrum, at veniam corrupti, vitae impedit, doloribus eius deserunt consequatur molestiae nihil.
+    Quae optio eum nisi fugiat corporis, amet doloribus enim quos aperiam error ipsum id consequuntur, omnis nihil! Expedita dolorem ex, voluptas officiis laboriosam sint odit alias officia, consectetur suscipit necessitatibus!
+    Eligendi excepturi cumque, alias ipsam modi tempore magnam fugit repellat rerum ipsum provident ipsa eius corporis nemo adipisci sint doloribus dolor ab error ratione fugiat. Praesentium nesciunt incidunt consequatur earum.
+    Labore, minima maxime iusto dolorem nihil unde sed repellat. Ad, sed qui quia recusandae aliquid fugiat doloribus eum explicabo corporis id suscipit placeat laudantium nobis similique soluta debitis eius aperiam?
+    Veritatis aspernatur architecto deleniti ullam, cum consectetur natus. Quae modi, nihil esse dolor dignissimos magnam, neque, repellat nisi excepturi voluptatibus amet libero. Modi necessitatibus animi natus ipsa, obcaecati distinctio aut.
+    Iste, consectetur voluptates quidem inventore porro in sapiente, dolores similique temporibus vitae aspernatur laudantium reprehenderit. Vitae cupiditate dolore nostrum, fugiat beatae a illum esse. Aspernatur similique fuga obcaecati enim explicabo!
+    Soluta quae est vitae nesciunt aliquam quibusdam non numquam doloribus optio quia ratione dolorum enim id dignissimos vel quas modi cupiditate omnis, error et. Animi unde deserunt nihil quis dolorem.
+    Eius recusandae voluptatem ut architecto ex, ratione hic iusto, harum adipisci voluptatum optio minima illum fuga unde ea animi. Neque quasi a quod quo optio cupiditate. Quam eum officia rerum.</Link>
+  </div>;
+}
